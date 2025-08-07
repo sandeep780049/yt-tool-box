335372f1c33b442074e1c54948123a9b9cc216da
sandeep780049 <anujsandeepyadav@gmail.com>
2025-08-07 16:56:28 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,19 +2,35 @@ from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(name)
 
-@app.route('/') def home(): return render_template('index.html')
+@app.route('/')
+def home():
+    return render_template('index.html')
+@app.route('/thumbnail')
+def thumbnail():
+  return render_template('thumbnail.html')
 
-@app.route('/thumbnail') def thumbnail(): return render_template('thumbnail.html')
+@app.route('/stats')
+def stats():
+  return render_template('stats.html')
 
-@app.route('/stats') def stats(): return render_template('stats.html')
+@app.route('/ai-title-description')
+def ai_title():
+  return render_template('ai_title.html')
 
-@app.route('/ai-title-description') def ai_title(): return render_template('ai_title.html')
+@app.route('/faq')
+def faq():
+  return render_template('faq.html')
 
-@app.route('/faq') def faq(): return render_template('faq.html')
+@app.route('/what-is-tag') 
+def what_is_tag(): 
+  return render_template('what_is_tag.html')
 
-@app.route('/what-is-tag') def what_is_tag(): return render_template('what_is_tag.html')
+@app.route('/what-is-thumbnail') 
+def what_is_thumbnail():
+  return render_template('what_is_thumbnail.html')
 
-@app.route('/what-is-thumbnail') def what_is_thumbnail(): return render_template('what_is_thumbnail.html')
+@app.route('/about-site')
+def about_site():
+  return render_template('about_site.html')
 
-@app.route('/about-site') def about_site(): return render_template('about_site.html')
 
