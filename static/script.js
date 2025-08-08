327c3af992880843bdde22f8760975c4e906f5b7
sandeep780049 <anujsandeepyadav@gmail.com>
2025-08-08 22:10:57 +0530

    Update script.js

--- static/script.js
+++ static/script.js
@@ -1,40 +1,5 @@
-// Copy to clipboard
-function copyToClipboard(elementId) {
-    const text = document.getElementById(elementId).innerText;
-    navigator.clipboard.writeText(text).then(() => {
-        alert("Copied to clipboard!");
-    });
-}
-
-// Download content
-function downloadContent(elementId, filename) {
-    const text = document.getElementById(elementId).innerText;
-    const blob = new Blob([text], { type: "text/plain" });
-    const url = window.URL.createObjectURL(blob);
-
-    const a = document.createElement("a");
-    a.href = url;
-    a.download = filename;
-    a.click();
-
-    window.URL.revokeObjectURL(url);
-}
-
-// Loader show/hide (if you want to use a loading spinner in future)
-function showLoader() {
-    const loader = document.getElementById("loader");
-    if (loader) loader.style.display = "block";
-}
-
-function hideLoader() {
-    const loader = document.getElementById("loader");
-    if (loader) loader.style.display = "none";
-}
-
-// Scroll to section
-function scrollToSection(sectionId) {
-    const element = document.getElementById(sectionId);
-    if (element) {
-        element.scrollIntoView({ behavior: "smooth" });
-    }
+// static/script.js
+function toggleMenu() {
+  const menu = document.getElementById("navMenu");
+  menu.classList.toggle("active");
 }
