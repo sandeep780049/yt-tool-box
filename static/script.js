a4e646454ff98b0646554f533f11901fa69aa6b0
sandeep780049 <anujsandeepyadav@gmail.com>
2025-08-08 22:24:20 +0530

    Update script.js

--- static/script.js
+++ static/script.js
@@ -3,3 +3,8 @@ function toggleMenu() {
   const menu = document.getElementById("navMenu");
   menu.classList.toggle("active");
 }
+function copyToClipboard(text) {
+  navigator.clipboard.writeText(text).then(() => {
+    alert("Copied to clipboard!");
+  });
+}
